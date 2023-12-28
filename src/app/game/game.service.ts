d51a35c8de4116bc321221f1ab1f14f0238558b2
David Batista <dbatista.t@gmail.com>
2023-12-28 15:36:32 -0400

    add environment for dev and prd

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -2,38 +2,33 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {firstValueFrom, Observable} from 'rxjs';
 import {Game, GameDetail, GameFilter} from "./game.interfaces";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  private apiUrl = 'https://free-to-play-games-database.p.rapidapi.com/api'
+  private apiUrl = environment.gameApi;
 
   constructor(private http: HttpClient) {
-    this.http.options('', {
-
-    })
+    this.http.options('', {})
   }
 
   getGames(filter: GameFilter): Promise<Game[]> {
     const params = {
       platform: filter.platform,
-      ...(filter.category !== 'all' && { category: filter.category }),
+      ...(filter.category !== 'all' && {category: filter.category}),
       ['order-by']: filter.order
     }
 
-    console.log({params})
-
-    return firstValueFrom(this.http.get<Game[]>(`${this.apiUrl}/games`, {headers: {
-        'X-RapidAPI-Key': '5b860c7b17msh5714a32ea6a1de4p110580jsnc80dccd9ceec',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-      }, params}));
+    return firstValueFrom(this.http.get<Game[]>(`${this.apiUrl}/games`, {
+      headers: environment.gameApiHeaders, params
+    }));
   }
 
   getGameById(id: number): Promise<GameDetail> {
-    return firstValueFrom(this.http.get<GameDetail>(`${this.apiUrl}/game?id=${id}`, {headers: {
-        'X-RapidAPI-Key': '5b860c7b17msh5714a32ea6a1de4p110580jsnc80dccd9ceec',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-      }}));
+    return firstValueFrom(this.http.get<GameDetail>(`${this.apiUrl}/game?id=${id}`, {
+      headers: environment.gameApiHeaders
+    }));
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// Update this when exist credentials for production
+export const environment = {
+  gameApi: 'https://free-to-play-games-database.p.rapidapi.com/api',
+  gameApiHeaders: {
+    'X-RapidAPI-Key': '5b860c7b17msh5714a32ea6a1de4p110580jsnc80dccd9ceec',
+    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+  }
+};
